0b80dd1423b55cfeea104d1ceb6e1fb721838cda
Andre Alvim <andre@alvim.net>
2019-09-17 19:06:54 -0300

    Configurando estrutura

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,7 @@ const { Router } = require("express");
 const routes = new Router();
 
 routes.get("/", (req, res) => {
-  return res.json({ message: "Ok" });
+  return res.json({ message: " Ok!" });
 });
 
 module.exports = routes;
